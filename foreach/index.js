712a6f31f9a8a76f2c3ff0db976293a49169d815
Harshda Mhase <harshadamhase12@gmail.com>
2023-10-19 06:13:42 -0400

    foreach implemented

---
+++ foreach/index.js
@@ -0,0 +1,5 @@
+const student = ["Ketan","Aditya","Saurbh","Rohan"];
+
+student.forEach(student => {
+  console.log(`Students in school: ${student[i]}`);  
+});
--- index2.js
+++ index2.js
@@ -1,5 +1,7 @@
-const sum = (a, b) => {
-    c = a + b;
-    console.log(c);
-}
-sum(2,3);
+// const sum = (a, b) => {
+//     c = a + b;
+//     console.log(c);
+// }
+// sum(2,3);
+
+
